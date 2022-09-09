8aea5cd9acf926e007bdfccc17c1989ab01f9e1d
2883857@student.edu.az <2883857@student.edu.az>
2022-09-09 20:29:30 +0400

    finish

--- Blink/assets/js/script.js
+++ Blink/assets/js/script.js
@@ -71,4 +71,49 @@ list.forEach((list1) => {
     list1.querySelector(".double-ul").classList.toggle("listactive")
     list1.querySelector("i").classList.toggle("iactive")
   })
-}) 
+})
+
+
+// Statics 
+
+// let counts = document.querySelectorAll('.count')
+
+// window.addEventListener('load', () => {
+//   counts.forEach((count) => {
+//     var countinner = count.innerText
+//     for (let i = 0; i <= countinner; i += 5) {
+//       setInterval(() => {
+//         if (i < countinner) {
+//           count.innerText = i;
+
+//         }
+
+//       }, 100)
+
+//     }
+//   })
+// })
+$(window).on("load resize", function () {
+
+  var counters = $(".count");
+  var countersQuantity = counters.length;
+  var counter = [];
+
+  for (i = 0; i < countersQuantity; i++) {
+    counter[i] = parseInt(counters[i].innerHTML);
+  }
+
+  var count = function (start, value, id) {
+    var localStart = start;
+    setInterval(function () {
+      if (localStart < value) {
+        localStart++;
+        counters[id].innerHTML = localStart;
+      }
+    }, 4);
+  }
+
+  for (j = 0; j < countersQuantity; j++) {
+    count(0, counter[j], j);
+  }
+});
